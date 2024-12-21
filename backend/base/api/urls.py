9e265a7a107e402ff8baca0fb76935d8e79a10b0
MOhit <mohit.pythondev@gmail.com>
2024-12-21 17:56:28 +0530

    Adding simple JWT

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from base.api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
